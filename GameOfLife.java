f0491d604ff82c2e7159d8a6dd4f3c32e7ebe9b6
DorB2307 <dor.barnoach@post.runi.ac.il>
2024-01-15 16:42:44 +0200

    Testing

--- GameOfLife.java
+++ GameOfLife.java
@@ -144,8 +144,8 @@ public class GameOfLife {
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     static void print(int[][] arr) {
 		int N = arr.length, M = arr[0].length;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
+		for(int i = 1; i < N - 1; i++) {
+			for(int j = 1; j < M - 1; j++) {
 				System.out.printf("%3s", arr[i][j]);
 			}
 			System.out.println();
